5774e336362dce057ccb3212709ab0263dbeb517
Geoff Ren <geren@microsoft.com>
2019-02-26 11:11:36 -0800

    updated REST points

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -28,11 +28,6 @@ module.exports = app => {
     app.post('/connector/functions', (req, res) => {
         console.log("~~~POST FUNCTIONS~~~");
         if (!req.body || !req.body.script) {
-            console.log(req.body === undefined)
-            if (req.body !== undefined) {
-                console.log(req.body)
-                console.log(req.body.script === undefined)
-            }
             res.status(404).send({error: 'no data'});
         } else {
             console.log(req.body.script);
@@ -40,9 +35,26 @@ module.exports = app => {
         }
     });    
 
-    // GET point to get all home ids from our database.
-    app.get('/connector/execute', (req, res) => {
-        console.log("~~~EXECUTE~~~");
-        res.status(200).send({success: "EXECUTE"})
-    });
+    // POST point to get all home ids from our database.
+    app.post('/connector/scripts', (req, res) => {
+        console.log("~~~POST SCRIPTS~~~");
+        if (!req.body || !req.body.user) {
+            res.status(404).send({error: 'no data'});
+        } else {
+            console.log(req.body.script);
+            res.status(200).send({scripts: ["script1, script2"]})
+        }
+    });    
+    
+
+    // POST point to get all home ids from our database.
+    app.post('/connector/execute', (req, res) => {
+        console.log("~~~POST EXECUTE~~~");
+        if (!req.body || !req.body.function) {
+            res.status(404).send({error: 'no data'});
+        } else {
+            console.log(req.body.function);
+            res.status(200).send({success: "succeeded"})
+        }
+    });  
 };
