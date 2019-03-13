1e04ca5565d9b03f186dd8a0a2c80556bd3da459
Geoff Ren <geren@microsoft.com>
2019-03-13 09:35:43 -0700

    updated connector.js

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -2,37 +2,33 @@
 
 module.exports = app => {
 
-    app.post('/connector/scripts', (req, res) => {
-        console.log("~~~POST SCRIPTS~~~");
-        if (!req.body || !req.body.location) {
-            res.status(404).send({error: 'no data'});
-        } else {
-            var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
-            const graphUrl = `https://graph.microsoft.com/v1.0/me`
-            const graphHttp = new XMLHttpRequest()
-            graphHttp.open("GET", graphUrl)
-            graphHttp.setRequestHeader("Authorization", req.headers.authorization);
-            graphHttp.send();
-            graphHttp.onreadystatechange=(e)=>{
-                if (graphHttp.readyState === graphHttp.DONE) {
-                    const userId = JSON.parse(graphHttp.responseText).id;
-                    console.log(JSON.parse(graphHttp.responseText))
-                    console.log("\n~~~~~~~~~~~~~~\n")
-                    console.log(req.headers.authorization);
-                    console.log("\n~~~~~~~~~~~~~~~~\n");
-                    console.log(req.body.location);
-                    if (req.body.location.toLowerCase() === "redmond") {
-                        if (userId === "1c889869-3278-480c-a242-7969a8224162") {
-                            res.status(200).send({scripts: [{script: "gerenRedmondScript1"}, {script: "gerenRedmondScript2"}]})
-                        } else {
-                            res.status(200).send({scripts: [{script: "someoneRedmondScript1"}, {script: "someoneRedmondScript2"}]})
-                        }
+    app.get('/connector/scripts', (req, res) => {
+        console.log("~~~GET SCRIPTS~~~");
+        var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+        const graphUrl = `https://graph.microsoft.com/v1.0/me`
+        const graphHttp = new XMLHttpRequest()
+        graphHttp.open("GET", graphUrl)
+        graphHttp.setRequestHeader("Authorization", req.headers.authorization);
+        graphHttp.send();
+        graphHttp.onreadystatechange=(e)=>{
+            if (graphHttp.readyState === graphHttp.DONE) {
+                const userId = JSON.parse(graphHttp.responseText).id;
+                // console.log(JSON.parse(graphHttp.responseText))
+                // console.log("\n~~~~~~~~~~~~~~\n")
+                // console.log(req.headers.authorization);
+                // console.log("\n~~~~~~~~~~~~~~~~\n");
+                console.log(JSON.stringify(req.query, null, 2));
+                if (req.query.location.toLowerCase() === "redmond") {
+                    if (userId === "1c889869-3278-480c-a242-7969a8224162") {
+                        res.status(200).send({scripts: [{script: "gerenRedmondScript1"}, {script: "gerenRedmondScript2"}]})
                     } else {
-                        if (userId === "1c889869-3278-480c-a242-7969a8224162") {
-                            res.status(200).send({scripts: [{script: "gerenScript1"}, {script: "gerenScript2"}]})
-                        } else {
-                            res.status(200).send({scripts: [{script: "script1"}, {script: "script2"}]})
-                        }
+                        res.status(200).send({scripts: [{script: "someoneRedmondScript1"}, {script: "someoneRedmondScript2"}]})
+                    }
+                } else {
+                    if (userId === "1c889869-3278-480c-a242-7969a8224162") {
+                        res.status(200).send({scripts: [{script: "gerenScript1"}, {script: "gerenScript2"}]})
+                    } else {
+                        res.status(200).send({scripts: [{script: "script1"}, {script: "script2"}]})
                     }
                 }
             }
