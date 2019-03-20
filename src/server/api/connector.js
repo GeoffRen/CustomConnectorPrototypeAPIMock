f16b5486c5b3edd92126541b31c239364fc39be8
Geoff Ren <geren@microsoft.com>
2019-03-20 08:25:42 -0700

    updated connector.js

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -12,6 +12,16 @@ module.exports = app => {
         res.sendStatus(200);
     });
 
+    app.get('/test/:drive/:file', (req, res) => {
+        console.log("~~~GET TEST OPERATION~~~");
+        console.log(`RECEIVED QUERY: ${JSON.stringify(req.query, null, 2)}`);
+        console.log(`RECEIVED PARAM: ${JSON.stringify(req.params, null, 2)}`);
+        console.log(`RECEIVED BODY: ${JSON.stringify(req.body, null, 2)}`);
+        console.log(req.headers.authorization);
+        // res.status(200).send(JSON.parse('{"success": "succeeded"}'));
+        res.sendStatus(200);
+    });
+
     app.get('/connector/scripts', (req, res) => {
         console.log("~~~GET SCRIPTS~~~");
         console.log(`RECEIVED QUERY: ${JSON.stringify(req.query, null, 2)}`);
