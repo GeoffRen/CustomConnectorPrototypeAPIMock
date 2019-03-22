51b9ab735b41dda84fbd650b7361bc3da56c1f36
Geoff Ren <geren@microsoft.com>
2019-03-22 09:24:20 -0700

    updated connector.js

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -108,67 +108,34 @@ module.exports = app => {
         console.log(`RECEIVED QUERY: ${JSON.stringify(req.query, null, 2)}`);
         console.log(`RECEIVED PARAM: ${JSON.stringify(req.params, null, 2)}`);
         console.log(`RECEIVED BODY: ${JSON.stringify(req.body, null, 2)}`);
-        var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
-        const graphUrl = `https://graph.microsoft.com/v1.0/me`
-        const graphHttp = new XMLHttpRequest()
-        graphHttp.open("GET", graphUrl)
-        graphHttp.setRequestHeader("Authorization", req.headers.authorization);
-        graphHttp.send();
-        graphHttp.onreadystatechange=(e)=>{
-            if (graphHttp.readyState === graphHttp.DONE) {
-                const userId = JSON.parse(graphHttp.responseText).id;
+        // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+        // const graphUrl = `https://graph.microsoft.com/v1.0/me`
+        // const graphHttp = new XMLHttpRequest()
+        // graphHttp.open("GET", graphUrl)
+        // graphHttp.setRequestHeader("Authorization", req.headers.authorization);
+        // graphHttp.send();
+        // graphHttp.onreadystatechange=(e)=>{
+        //     if (graphHttp.readyState === graphHttp.DONE) {
+        //         const userId = JSON.parse(graphHttp.responseText).id;
                 // console.log(JSON.parse(graphHttp.responseText))
                 // console.log("\n~~~~~~~~~~~~~~\n")
                 // console.log(req.headers.authorization);
                 // console.log("\n~~~~~~~~~~~~~~~~\n");
-                console.log(JSON.stringify(req.query, null, 2));
-                if (req.body.location.toLowerCase() === "redmond") {
-                    if (userId === "1c889869-3278-480c-a242-7969a8224162") {
-                        res.status(200).send({scripts: [{script: "gerenRedmondScript1"}, {script: "dynamicSchema0"}]})
-                    } else {
+                if (req.query.location.toLowerCase() === "redmond") {
+                    // if (userId === "1c889869-3278-480c-a242-7969a8224162") {
+                    //     res.status(200).send({scripts: [{script: "gerenRedmondScript1"}, {script: "dynamicSchema0"}]})
+                    // } else {
                         res.status(200).send({scripts: [{script: "someoneRedmondScript1"}, {script: "dynamicSchema1"}]})
-                    }
+                    // }
                 } else {
-                    if (userId === "1c889869-3278-480c-a242-7969a8224162") {
-                        res.status(200).send({scripts: [{script: "gerenScript1"}, {script: "dynamicSchema2"}]})
-                    } else {
+                    // if (userId === "1c889869-3278-480c-a242-7969a8224162") {
+                    //     res.status(200).send({scripts: [{script: "gerenScript1"}, {script: "dynamicSchema2"}]})
+                    // } else {
                         res.status(200).send({scripts: [{script: "script1"}, {script: "dynamicSchema3"}]})
-                    }
+                    // }
                 }
-            }
-        }
-    });
-
-    app.get('/connector/returntest0', (req, res) => {
-        console.log("~~~GET RET TEST 0 OPERATION~~~");
-        console.log(`RECEIVED QUERY: ${JSON.stringify(req.query, null, 2)}`);
-        console.log(`RECEIVED PARAM: ${JSON.stringify(req.params, null, 2)}`);
-        console.log(`RECEIVED BODY: ${JSON.stringify(req.body, null, 2)}`);
-        res.status(200).send("string ret test");
-    });
-
-    app.get('/connector/returntest1', (req, res) => {
-        console.log("~~~GET RET TEST 1 OPERATION~~~");
-        console.log(`RECEIVED QUERY: ${JSON.stringify(req.query, null, 2)}`);
-        console.log(`RECEIVED PARAM: ${JSON.stringify(req.params, null, 2)}`);
-        console.log(`RECEIVED BODY: ${JSON.stringify(req.body, null, 2)}`);
-        res.status(200).send({value0: "value 0 ret test"});
-    });
-
-    app.get('/connector/returntest2', (req, res) => {
-        console.log("~~~GET RET TEST 2 OPERATION~~~");
-        console.log(`RECEIVED QUERY: ${JSON.stringify(req.query, null, 2)}`);
-        console.log(`RECEIVED PARAM: ${JSON.stringify(req.params, null, 2)}`);
-        console.log(`RECEIVED BODY: ${JSON.stringify(req.body, null, 2)}`);
-        res.status(200).send({value0: "value 0 ret test", value1: 25});
-    });
-
-    app.post('/connector/execute', (req, res) => {
-        console.log("~~~GET EXECUTE OPERATION~~~");
-        console.log(`RECEIVED QUERY: ${JSON.stringify(req.query, null, 2)}`);
-        console.log(`RECEIVED PARAM: ${JSON.stringify(req.params, null, 2)}`);
-        console.log(`RECEIVED BODY: ${JSON.stringify(req.body, null, 2)}`);
-        res.status(200).send({success: "succeeded"});
+            // }
+        // }
     });
 
     app.post('/connector/user', (req, res) => {
