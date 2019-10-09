55fa404b2eac0b2b9f73a866113fa1677216ae09
Geoff Ren <geren@microsoft.com>
2019-10-09 13:25:08 -0700

    updated connector.js

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -660,7 +660,7 @@ module.exports = app => {
         console.log(`RECEIVED PARAM: ${JSON.stringify(req.params, null, 2)}`);
         console.log(`RECEIVED BODY: ${JSON.stringify(req.body, null, 2)}`);
         console.log(req.headers);
-        res.status(200).send([
+        res.status(200).send(
             // {
             //     // metadata: {
             //         name: "geoffScript",
@@ -730,7 +730,7 @@ module.exports = app => {
                     }
                 }
             }
-        ]);
+        );
     });
 
     app.post("/connector/user", (req, res) => {
