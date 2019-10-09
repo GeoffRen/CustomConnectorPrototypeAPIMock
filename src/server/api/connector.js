578473f23a86c3ce3f907d606ffe5a518626cdcc
Geoff Ren <geren@microsoft.com>
2019-10-09 15:32:31 -0700

    updated connector.js

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -742,58 +742,7 @@ module.exports = app => {
                             }
                         }
                     }
-                },
-                flowParameterSchema: {
-                    type: "object",
-                    required: ["p0", "p2"],
-                    properties: {
-                        p0: {
-                            type: "String"
-                        },
-                        p1: {
-                            type: "Number",
-                            default: 5
-                        },
-                        p2: {
-                            type: "Array",
-                            items: {
-                                type: "String"
-                            }
-                        },
-                        p3: {
-                            type: "Boolean"
-                        }
-                    }
-                },
-                flowReturnSchema: {
-                    type: "object",
-                    properties: {
-                        result: {
-                            type: "Object",
-                            properties: {
-                                r0: {
-                                    type: "String"
-                                },
-                                r1: {
-                                    type: "Object",
-                                    properties: {
-                                        r2: {
-                                            type: "Object",
-                                            properties: {
-                                                r3: {
-                                                    type: "Number"
-                                                }
-                                            }
-                                        }
-                                    }
-                                },
-                                r4: {
-                                    type: "Boolean"
-                                }
-                            }
-                        }
-                    }
-                }                
+                }
             }
         );
     });
