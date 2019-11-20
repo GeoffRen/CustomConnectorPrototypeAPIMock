ace31056519c98f2bfe2921bdad50eba708c50ba
Geoff Ren <geren@microsoft.com>
2019-11-20 08:41:17 -0800

    updated connector.js

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -704,7 +704,7 @@ module.exports = app => {
         console.log(`RECEIVED PARAM: ${JSON.stringify(req.params, null, 2)}`);
         console.log(`RECEIVED BODY: ${JSON.stringify(req.body, null, 2)}`);
         // console.log(req.headers);
-        if (req.query.scriptId < 6 || req.params.scriptId < 6) {
+        if ((req.query.scriptId && req.query.scriptId < 6) || (req.params && req.params.scriptId < 6)) {
             console.log('~~~HAS STUFF~~~');
             res.status(200).send({
                 flowParameterSchema: {
