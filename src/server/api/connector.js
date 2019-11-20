bcdbc15055a68d7e94bc158c3ce8d050c8a12655
Geoff Ren <geren@microsoft.com>
2019-11-20 10:22:23 -0800

    updated connector.js

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -728,21 +728,8 @@ module.exports = app => {
                 "parameterInfo":"{\"originalParameterOrder\":[],\"flowParameterSchema\":null,\"flowReturnSchema\":null}",
                 // "parameterInfo":"{\"originalParameterOrder\":[],\"flowParameterSchema\":{\"type\":\"object\",\"properties\":{\"testtesttest\":{\"type\":\"string\",\"default\":\"testest\",\"x-ms-visibility\":\"important\"}}},\"flowReturnSchema\":{\"type\":\"object\",\"properties\":{},\"x-ms-visibility\":\"internal\"}}"
                 parameterInfo: {
-                    flowParameterSchema: {
-                        type: "object",
-                        properties: {
-                            dummyParameter: {
-                                type: "string",
-                                "x-ms-visibility": "important",
-                                default: 'dummyVal'
-                            }
-                        }
-                    },
-                    flowReturnSchema: {
-                        type: "object",
-                        properties: {},
-                        "x-ms-visibility": "internal"
-                    },
+                    flowParameterSchema: null,
+                    flowReturnSchema: null,
                 }
             });
         } else {
