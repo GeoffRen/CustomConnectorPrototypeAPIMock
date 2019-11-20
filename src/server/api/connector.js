1745ec5386c8f4a2698f7f79e2ddb03da6f03204
Geoff Ren <geren@microsoft.com>
2019-11-20 10:15:46 -0800

    updated connector.js

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -723,6 +723,10 @@ module.exports = app => {
                     properties: {},
                     "x-ms-visibility": "internal"
                 },
+                parameterInfo: {
+                    flowParameterSchema: null,
+                    flowReturnSchema: null,
+                },
                 "parameterInfo":"{\"originalParameterOrder\":[],\"flowParameterSchema\":{\"type\":\"object\",\"properties\":{\"dummyParameter\":{\"type\":\"string\",\"default\":\"dummyVal\",\"x-ms-visibility\":\"internal\"}}},\"flowReturnSchema\":{\"type\":\"object\",\"properties\":{},\"x-ms-visibility\":\"internal\"}}"
             });
         } else {
@@ -745,9 +749,25 @@ module.exports = app => {
                 //     properties: {},
                 //     "x-ms-visibility": "internal"
                 // },
-                "parameterInfo":"{\"originalParameterOrder\":[],\"flowParameterSchema\":null,\"flowReturnSchema\":null}"
+                "parameterInfo":"{\"originalParameterOrder\":[],\"flowParameterSchema\":null,\"flowReturnSchema\":null}",
                 // "parameterInfo":"{\"originalParameterOrder\":[],\"flowParameterSchema\":{\"type\":\"object\",\"properties\":{\"testtesttest\":{\"type\":\"string\",\"default\":\"testest\",\"x-ms-visibility\":\"important\"}}},\"flowReturnSchema\":{\"type\":\"object\",\"properties\":{},\"x-ms-visibility\":\"internal\"}}"
-                // parameterInfo: ""
+                parameterInfo: {
+                    flowParameterSchema: {
+                        type: "object",
+                        properties: {
+                            dummyParameter: {
+                                type: "string",
+                                "x-ms-visibility": "important",
+                                default: 'dummyVal'
+                            }
+                        }
+                    },
+                    flowReturnSchema: {
+                        type: "object",
+                        properties: {},
+                        "x-ms-visibility": "internal"
+                    },
+                }
                 // parameterInfo: JSON.stringify({
                 //     originalParameterOrder: [
                 //         {
