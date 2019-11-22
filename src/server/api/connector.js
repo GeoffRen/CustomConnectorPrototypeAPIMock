a2a2dda78c73e1ff2997e764b001efbb9aea38f9
Geoff Ren <geren@microsoft.com>
2019-11-22 09:19:02 -0800

    updated connector.js

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -774,27 +774,27 @@ module.exports = app => {
                 //     "x-ms-visibility": "internal"
                 // },
                 // "parameterInfo":"{\"originalParameterOrder\":[],\"flowParameterSchema\":null,\"flowReturnSchema\":null}",
-                // "parameterInfo":"{\"originalParameterOrder\":[],\"flowParameterSchema\":{\"type\":\"object\",\"properties\":{\"testtesttest\":{\"type\":\"string\",\"default\":\"testest\",\"x-ms-visibility\":\"important\"}}},\"flowReturnSchema\":{\"type\":\"object\",\"properties\":{},\"x-ms-visibility\":\"internal\"}}"
-                parameterInfo: {
-                    flowParameterSchema: {
-                        type: "object",
-                        properties: {
-                            serverTest: {
-                                type: "string",
-                                "x-ms-visibility": "important",
-                                default: 'serverVal'
-                            }
-                        }
-                    },
-                    flowReturnSchema: {
-                        type: "object",
-                        properties: {
-                            ret: {
-                                type: "string"
-                            }
-                        }
-                    },
-                }
+                "parameterInfo":"{\"originalParameterOrder\":[],\"flowParameterSchema\":{\"type\":\"object\",\"properties\":{\"testtesttest\":{\"type\":\"string\",\"default\":\"testest\",\"x-ms-visibility\":\"important\"}}},\"flowReturnSchema\":{\"type\":\"object\",\"properties\":{},\"x-ms-visibility\":\"internal\"}}"
+                // parameterInfo: {
+                //     flowParameterSchema: {
+                //         type: "object",
+                //         properties: {
+                //             serverTest: {
+                //                 type: "string",
+                //                 "x-ms-visibility": "important",
+                //                 default: 'serverVal'
+                //             }
+                //         }
+                //     },
+                //     flowReturnSchema: {
+                //         type: "object",
+                //         properties: {
+                //             ret: {
+                //                 type: "string"
+                //             }
+                //         }
+                //     },
+                // }
             });
         }
     });
