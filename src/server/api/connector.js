f2b09cf9d4a00b2405a907caaf20a72739216703
Geoff Ren <geren@microsoft.com>
2019-12-31 11:55:43 -0800

    updated connector.js

--- src/server/api/connector.js
+++ src/server/api/connector.js
@@ -420,7 +420,7 @@ module.exports = app => {
             res.setHeader('geoff', 'geoff header');
             res.setHeader('x-ms-client-request-id', 'noParametersRequestId');
             res.status(200).send({
-                result: 0,
+                result: "",
                 logs: []
             });
         } else {
@@ -742,7 +742,7 @@ module.exports = app => {
                         type: "object",
                         properties: {
                             result: {
-                                type: "number"
+                                type: "string"
                             }
                         }
                     }
